a3b607e1ee5c990bd1feffcaf64a219554c9f5c7
Athrva Deshmukh <athrvadeshmukh21@gmail.com>
2024-10-02 19:35:52 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum 
+{
+    public static void main(String args[])
+    {
+        int a = 100;
+        int b = 700;
+        int c = a+b;
+
+        int result = c;
+
+        System.out.println("The sum of a+b is : " + result);
+    }
+}
